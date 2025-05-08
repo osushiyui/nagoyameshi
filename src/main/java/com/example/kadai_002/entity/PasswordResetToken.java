baae1e2751c84a6360de7c73b7a0a97b2220bf63
osushiyui <ky.ks.fh.04@gmail.com>
2025-05-08 21:21:50 +0900

    Add files via upload

---
+++ src/main/java/com/example/kadai_002/entity/PasswordResetToken.java
@@ -0,0 +1,27 @@
+package com.example.kadai_002.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class PasswordResetToken {
+	@Id
+    @GeneratedValue
+    private Long id;
+
+    private String token;
+    
+    @OneToOne
+    private User user;
+
+    public PasswordResetToken() {}
+
+    public PasswordResetToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+    }
+}
